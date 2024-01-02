11127254446586b8b93cfbe9a2f7b18ccc9e38f7
Nick Ketter <nketter@gmail.com>
2024-01-01 21:33:27 -0800

    ok, function.js is really done.

--- functions.js
+++ functions.js
@@ -1,66 +1,84 @@
 import 'dotenv/config';
-import {openai} from './openai.js';
+import { openai } from './openai.js';
 import math from 'advanced-calculator';
 
 const QUESTION = process.argv[2] || 'hi';
 
 const messages = [
-    {
-        role: 'user',
-        content: QUESTION,
-    }
+  {
+    role: 'user',
+    content: QUESTION,
+  },
 ];
 
 const functions = {
-    calculate ({expression})
-    {
-        return math.evaluate(expression);
-    },
-}
+  calculate({ expression }) {
+    return math.evaluate(expression);
+  },
+  async generateImage({ prompt }) {
+    const results = await openai.images.generate({ prompt });
+    console.log(results);
+    return ' ';
+  },
+};
 
-const getCompletion = async (messages) => {
-    const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo-0613',
-      messages,
-      functions: [
-        {
-          name: 'calculate',
-          description: 'Run a math expression',
-          parameters: {
-            type: 'object',
-            properties: {
-              expression: {
-                type: 'string',
-                description:
-                  'Then math expression to evaluate like "2 * 3 + (21 / 2) ^ 2"',
-              },
+const getCompletion = async messages => {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-0613',
+    messages,
+    functions: [
+      {
+        name: 'calculate',
+        description: 'Run a math expression',
+        parameters: {
+          type: 'object',
+          properties: {
+            expression: {
+              type: 'string',
+              description:
+                'Then math expression to evaluate like "2 * 3 + (21 / 2) ^ 2"',
             },
-            required: ['expression'],
           },
+          required: ['expression'],
         },
-      ],
-      temperature: 0,
-    })
-  
-    return response
-  }
-  
+      },
+      {
+        name: 'generateImage',
+        description: 'Create or generate an image based on a description',
+        parameters: {
+          type: 'object',
+          properties: {
+            prompt: {
+              type: 'string',
+              description:
+                "THe description of the image you'd like to generate like 'A painting of a cat sitting on a chair'",
+            },
+          },
+          required: ['prompt'],
+        },
+      },
+    ],
+    temperature: 0,
+  });
 
-let response
+  return response;
+};
+
+let response;
 while (true) {
-  response = await getCompletion(messages)
+  response = await getCompletion(messages);
 
   if (response.choices[0].finish_reason === 'stop') {
-    console.log(response.choices[0].message.content)
-    break
+    console.log(response.choices[0].message.content);
+    break;
   } else if (response.choices[0].finish_reason === 'function_call') {
-    const fnName = response.choices[0].message.function_call.name
-    const args = response.choices[0].message.function_call.arguments
+    const fnName = response.choices[0].message.function_call.name;
+    const args = response.choices[0].message.function_call.arguments;
 
-    const functionToCall = functions[fnName]
-    const params = JSON.parse(args)
+    const functionToCall = functions[fnName];
+    const params = JSON.parse(args);
 
-    const result = functionToCall(params)
+    const result = functionToCall(params);
 
     messages.push({
       role: 'assistant',
@@ -69,12 +87,12 @@ while (true) {
         name: fnName,
         arguments: args,
       },
-    })
+    });
 
     messages.push({
       role: 'function',
       name: fnName,
       content: JSON.stringify({ result: result }),
-    })
+    });
   }
 }
